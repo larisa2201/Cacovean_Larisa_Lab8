b4b59d6ead6a23b69713b725c3eecdfd0b64d1a5
larisa.cacovean@gmail.com <larisa.cacovean@gmail.com>
2021-12-08 15:26:23 +0200

    Laboratorul9

--- Migrations/20211128175117_BookCategory.cs
+++ Migrations/20211128175117_BookCategory.cs
@@ -6,12 +6,6 @@ namespace Cacovean_Larisa_Lab8.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<int>(
-                name: "PublisherID",
-                table: "Book",
-                nullable: false,
-                defaultValue: 0);
-
             migrationBuilder.CreateTable(
                 name: "Category",
                 columns: table => new
@@ -25,18 +19,7 @@ namespace Cacovean_Larisa_Lab8.Migrations
                     table.PrimaryKey("PK_Category", x => x.ID);
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Publisher",
-                columns: table => new
-                {
-                    ID = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    PublisherName = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Publisher", x => x.ID);
-                });
+           
 
             migrationBuilder.CreateTable(
                 name: "BookCategory",
@@ -64,10 +47,6 @@ namespace Cacovean_Larisa_Lab8.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Book_PublisherID",
-                table: "Book",
-                column: "PublisherID");
 
             migrationBuilder.CreateIndex(
                 name: "IX_BookCategory_BookID",
@@ -79,13 +58,7 @@ namespace Cacovean_Larisa_Lab8.Migrations
                 table: "BookCategory",
                 column: "CategoryID");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_Book_Publisher_PublisherID",
-                table: "Book",
-                column: "PublisherID",
-                principalTable: "Publisher",
-                principalColumn: "ID",
-                onDelete: ReferentialAction.Cascade);
+            
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
